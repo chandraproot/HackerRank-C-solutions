04aeb5c8872a26e06eea51cf75e8453659cb41b5
Chandra Prakash <104268637+chandraproot@users.noreply.github.com>
2022-07-11 22:32:00 +0530

    Create 4, Functions in C.c

---
+++ 4, Functions in C.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int max_of_four(int a, int b, int c, int d)
+{
+    int temp;
+    if(a > b && a > c && a > d){
+        temp = a;
+    }
+    else if(b >a && b > c && b > d){
+        temp = b;
+    }
+    else if(c > a && c > b && c > d){
+        temp = c;
+    }
+    else{
+        temp = d;
+    }
+    return temp;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
